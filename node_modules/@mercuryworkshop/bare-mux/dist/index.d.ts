af1a2061b170d3aca38a097ebe290dd2bc0a72b4
Galactic Network <114846211+GalacticNetwork@users.noreply.github.com>
2024-07-06 23:54:14 +0000

    WE UP
    Co-authored-by: insert <insertdevs@users.noreply.github.com>

---
+++ node_modules/@mercuryworkshop/bare-mux/dist/index.d.ts
@@ -0,0 +1,11 @@
+export * from './BareTypes';
+export * from './BareClient';
+export * from './Switcher';
+export * from './RemoteClient';
+export { BareClient as default } from './BareClient';
+export { WebSocketFields } from "./snapshot";
+export type * from './BareTypes';
+export type * from './BareClient';
+export type * from './Switcher';
+export type * from './RemoteClient';
+export type * from "./snapshot";
---
+++ server/index.js
@@ -0,0 +1,55 @@
+import express from "express";
+import { createServer } from "node:http";
+import { fileURLToPath } from "url";
+import { uvPath } from "@titaniumnetwork-dev/ultraviolet";
+import { epoxyPath } from "@mercuryworkshop/epoxy-transport";
+import { baremuxPath } from "@mercuryworkshop/bare-mux/node";
+import { join } from "node:path";
+import { hostname } from "node:os";
+import wisp from "wisp-server-node"
+
+const __dirname = fileURLToPath(new URL(".", import.meta.url));
+const publicPath = join(__dirname, "../public");
+const app = express();
+
+app.use(express.static(publicPath));
+
+app.use("/uv/", express.static(uvPath));
+app.use("/epoxy/", express.static(epoxyPath));
+app.use("/baremux/", express.static(baremuxPath));
+
+app.use((req, res) => {
+    res.status(404);
+    res.sendFile(join(publicPath, "404.html"));
+  });
+
+const server = createServer();
+
+server.on("request", (req, res) => {
+  res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
+  res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
+  app(req, res);
+});
+server.on("upgrade", (req, socket, head) => {
+  if (req.url.endsWith("/wisp/"))
+    wisp.routeRequest(req, socket, head);
+  else
+    socket.end();
+});
+
+let port = parseInt(process.env.PORT || "");
+
+if (isNaN(port)) port = 8080;
+
+process.on("SIGINT", shutdown);
+process.on("SIGTERM", shutdown);
+
+function shutdown() {
+  console.log("SIGTERM signal received: closing HTTP server");
+  server.close();
+  process.exit(0);
+}
+
+server.listen({
+  port,
+});
