acc18217460e76740418bc70a0c98d3a6b4b7653
Galactic Network <114846211+GalacticNetwork@users.noreply.github.com>
2024-07-06 00:40:07 +0000

    nah

---
+++ public/resources/js/tip.js
@@ -0,0 +1,6 @@
+setTimeout(() => {
+    document.querySelector('.tip').classList.add('fade-out');
+}, 9000);
+setTimeout(() => {
+    document.querySelector('.tip').style.display = 'none';
+}, 10000);
