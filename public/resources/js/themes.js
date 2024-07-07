3f3c3d06f7b642dc0d968965b0ab3cd57b91b197
Galactic Network <114846211+GalacticNetwork@users.noreply.github.com>
2024-07-07 22:53:15 +0000

    i dontknow

--- public/resources/js/themes.js
+++ public/resources/js/themes.js
@@ -5,15 +5,14 @@ function setTheme(theme) {
 
 function initTheme() {
     const savedTheme = localStorage.getItem('theme') || 'main';
-    setTheme(savedTheme);
-}
-
-const themeSelect = document.getElementById('theme-select');
-if (themeSelect) {
-    themeSelect.addEventListener('change', () => {
-        const selectedTheme = themeSelect.value;
-        setTheme(selectedTheme);
-    });
+    document.body.setAttribute('theme', savedTheme);
+    const themeSelect = document.getElementById('theme-select');
+    if (themeSelect) {
+        themeSelect.value = savedTheme;
+        themeSelect.addEventListener('change', () => {
+            setTheme(themeSelect.value);
+        });
+    }
 }
 
 initTheme();
