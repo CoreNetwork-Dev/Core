924e50f035f3607503fcbf8bd2540f55ec845256
Galactic Network <114846211+GalacticNetwork@users.noreply.github.com>
2024-07-10 01:55:45 +0000

    y

--- public/resources/js/mathhelp/index.js
+++ public/resources/js/mathhelp/index.js
@@ -15,8 +15,6 @@ form.addEventListener("submit", async (event) => {
   event.preventDefault();
 
   try {
-    let wispUrl = (location.protocol === "https:" ? "wss" : "ws") + "://" + location.host + "/wisp/";
-    BareMux.SetTransport("EpxMod.EpoxyClient", { wisp: wispUrl });
     await registerSW();
   } catch (err) {
     console.log(err);
